6f944581fa48abd00be424efedbb93cc02a7fe78
bryanchen18 <bryanchen@email.virginia.edu>
2016-03-16 13:19:22 -0400

    added DESCRIPTION dependencies and chart.R helper functions

---
+++ R/chart.R
@@ -0,0 +1,62 @@
+#'
+#'Gets price change over period
+#'param START date format
+#'param END date format
+#'param DATAFRAME data frame over period
+#'returns price_change: difference in Cl over period
+#'
+GetPriceChange <- function(END, START, DATAFRAME) {
+  a <- DATAFRAME[DATAFRAME$date == START,1]
+  b <- DATAFRAME[DATAFRAME$date == END,1]
+  # if gold data at b returns numeric(0) i.e. at off hours, get data s.t. b = END-1, a = START-1
+  if (identical(b, numeric(0))) {
+    assign("a", DATAFRAME[DATAFRAME$date == (START-1),1])
+    assign("b", DATAFRAME[DATAFRAME$date == (END-1),1])
+    # if gold data at b still returns numeric(0) i.e. at off hours, get data s.t. b = END-2, a = START-2
+    if (identical(b, numeric(0))) {
+      assign("a", DATAFRAME[DATAFRAME$date == (START-2),1])
+      assign("b", DATAFRAME[DATAFRAME$date == (END-2),1])
+    }
+  }
+  price_change <- format(as.numeric(b-a), nsmall = 2)
+  return (price_change)
+}
+
+# Get end of previous week (Eopw)
+Eopw <- function(DAY) {
+  dates <- seq((Sys.Date()-7), (Sys.Date()-1), by="days")
+  return (dates[wday(dates, label = T)==DAY])
+}
+# Get end of previous month (Eopm)
+Eopm <- function(DAY) {
+  return (as.Date(format(DAY, "%Y-%m-01")) - 1)
+}
+
+# formats the date for Delt operation
+FormatDelt <- function(DATE) {
+  formatted_date <- paste(as.character(DATE), as.character(Sys.Date()), sep="::")
+  return (formatted_date)
+}
+
+# gets max in date range (i.e. one_mo, three_mo, etc)
+GetMax <- function (RANGE) {
+  return (max(gld_data[gld_data$date >= (RANGE),1], na.rm=TRUE))
+}
+
+# gets min in date range
+GetMin <- function (RANGE) {
+  return (min(gld_data[gld_data$date >= (RANGE),1], na.rm=TRUE))
+}
+
+#'
+#'
+#'
+SaveCharts <- function () {
+  # Default theme for all tableGrobs
+  k_theme <- gridExtra::ttheme_default(
+    core = list(fg_params=list(cex = 0.4)),
+    colhead = list(fg_params=list(cex = 0.4)),
+    rowhead = list(fg_params=list(cex = 0.4))
+  )
+  theme_set(theme_bw(base_size = 5))
+}
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # chartr
 chartr is a package for R aimed at making charts with detailed annotations of economic variables
 
-# Overview
+## Overview
 `chartr` will largely consist of "wrapper" / "convenience" functions utilizing the functions from active `R` packages such as `magrittr`, `quantmod`, and `ggplot2`.
+Allows lubridate more flexible date selection in graphs and backtesting
